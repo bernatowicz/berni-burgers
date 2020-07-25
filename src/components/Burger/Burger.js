42bd607530f74ea2e678a6b3f883deec9069fa22
Jim Bernatowicz <jim@bernatowicz.net>
2020-07-25 10:16:56 -0400

    Render dynamic ingredients driven by state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,42 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+  // Crazy complicated transformation below!
+  //
+  // Start by making an array of the object's keys to .map over.
+  // For each key (k) which is an ingredient type:
+  //   .map over an Array having a length matching the count of that ingredient (values, undefined!)
+  //   Because values of the array are undefined, so we don't care about the values, only the index i
+  //   Finally output the JSX component with a key prop having its type plus the index for uniquness
+  //   and its type prop = k.
+  //
+  // ingredients: {
+  //     salad: 1,
+  //     bacon: 2,
+  //     cheese: 2,
+  //     meat: 2,
+  //   },
+  //
+  // transforms into:
+  //
+  // <BurgerIngredient key="salad0" type="salad" />
+  // <BurgerIngredient key="bacon0" type="bacon" />
+  // <BurgerIngredient key="bacon1" type="bacon" />
+  // <BurgerIngredient key="cheese0" type="cheese" />
+  // <BurgerIngredient key="cheese1" type="cheese" />
+  // <BurgerIngredient key="meat0" type="meat" />
+  // <BurgerIngredient key="meat1" type="meat" />
+
+  const transformedIngredients = Object.keys(props.ingredients).map((k) => {
+    return [...Array(props.ingredients[k])].map((dontCare, i) => {
+      return <BurgerIngredient key={k + i} type={k} />;
+    });
+  });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="salad" />
-      <BurgerIngredient type="bacon" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="bacon" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
